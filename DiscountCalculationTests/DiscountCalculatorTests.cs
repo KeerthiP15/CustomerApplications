ebbffcf1baca2e71e0b06da4930eac0602a30712
Keerthi Mahalingam <keerthimahalingam1999@outlook.com>
2025-02-18 21:24:21 +0530

    Added discount calculation logic

---
+++ DiscountCalculationTests/DiscountCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using CustomerApplication.Controllers;
+using CustomerApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace DiscountCalculationTests
+{
+    public class DiscountCalculatorTests
+    {
+        [Fact]
+        public void CalculateDiscount_ValidInputs_ReturnsCorrectDiscount()
+        {
+            // Arrange
+            var calculator = new ProductController();
+    
+            var product = new ProductModel { OrderAmount = 100 };
+           
+            var result = calculator.CalculateDiscount(product) as ViewResult;
+            Assert.NotNull(result);  // Ensure it's not null
+
+            var result1 = result.Model as ProductModel;
+            Assert.NotNull(result1);  // Ensure model is not null
+
+            decimal productPrice = Convert.ToDecimal(result1.Total);
+            
+            // Assert
+            Assert.Equal(90, productPrice);
+        }
+
+       
+        
+    }
+}
