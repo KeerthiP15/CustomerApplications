ac01f166899eab87d0efdc1e1d64207f4437005a
Keerthi <Keerthi@LAPTOP-V7D88IMN>
2025-02-18 12:18:32 +0530

    Add project files.

---
+++ CustomerApplication/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomerApplication.Models;
+namespace CustomerApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult OrderEntry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CalculateDiscount(ProductModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return NoContent();
+            }
+            
+            if (model.OrderAmount >= 100)
+            {
+                model.DiscountPercent = 10;
+                model.DiscountAmount = (model.OrderAmount * model.DiscountPercent / 100);
+                model.Total = model.OrderAmount - model.DiscountAmount;
+            }
+            return View("ProductView", model);
+
+
+        }
+        public IActionResult ProductView(ProductModel model)
+        {
+            return View(model);
+        }
+
+    }
+}
---
+++ CustomerApplication/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+namespace CustomerApplication.Models
+{
+    public class ProductModel
+    {
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Order Amount must be between 1 and 1,00,000.")]
+
+        public decimal OrderAmount { get; set; }
+        public string? CustomerType { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal Total { get; set; }
+    }
+}
